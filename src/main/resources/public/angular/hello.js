b2dc18fe6bf953f81168bdae10da5c61c86b4cfb
adeady <angela.deady@perceptivesoftware.com>
2015-07-31 14:29:48 -0400

    add ui-grid

--- src/main/resources/public/angular/hello.js
+++ src/main/resources/public/angular/hello.js
@@ -1,6 +1,15 @@
-var myAppModule = angular.module('myApp', ['ui.bootstrap'])
+var myAppModule = angular.module('myApp', ['ui.bootstrap','ui.grid'])
     .controller('UsersController', function($scope, $http) {
         $http.get('/api/users').success(function(data) {
             $scope.names = data;
+            $scope.gridOptions = {
+                columnDefs: [
+                    {field: 'name', displayName: 'Name'},
+                    {
+                        displayName: 'Edit',
+                        cellTemplate: '<button id="editBtn" type="button" class="btn-small" >Edit</button> '
+                    }
+                ]
+            };
         });
     });
